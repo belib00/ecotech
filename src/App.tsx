73b2aed63f96d6ff42484be3a43f56c1fcc5cef0
belib00 <isabelabrandao926@gmail.com>
2026-05-28 20:45:05 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import { useEffect } from 'react';
-import { criarUsuario, buscarUsuarios } from './services/api';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Index from "./pages/Index";
+import Produtos from "./pages/Produtos";
+import ProdutoDetalhe from "./pages/ProdutoDetalhe";
+import Sobre from "./pages/Sobre";
+import Contato from "./pages/Contato";
+import NotFound from "./pages/NotFound";
 
 function App() {
-
-  useEffect(() => {
-
-    async function testarAPI() {
-
-      await criarUsuario(
-        'Isabela',
-        'isa@gmail.com'
-      );
-
-      const usuarios = await buscarUsuarios();
-
-      console.log(usuarios);
-    }
-
-    testarAPI();
-
-  }, []);
-
   return (
-    <div>
-      <h1>EcoTech</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/produtos" element={<Produtos />} />
+        <Route path="/produto/:id" element={<ProdutoDetalhe />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
