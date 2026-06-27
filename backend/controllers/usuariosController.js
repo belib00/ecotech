f13ecc12a1f23bfeefb1669d3704c47527b94e87
marialuizaraposo <marialuizatavaresraposo@gmail.com>
2026-06-27 08:30:38 -0300

    Add buscarPorId, atualizar, and deletar functions

--- backend/controllers/usuariosController.js
+++ backend/controllers/usuariosController.js
@@ -3,22 +3,66 @@ import usuariosService from "../services/usuariosService.js";
 async function listarTodos(req, res) {
   try {
     const usuarios = await usuariosService.listarTodos();
-    res.json(usuarios);
+    return res.json(usuarios);
   } catch (erro) {
-    res.status(500).json({ erro: erro.message });
+    return res.status(500).json({ erro: erro.message });
   }
 }
 
 async function criar(req, res) {
   try {
     const usuario = await usuariosService.criar(req.body);
-    res.status(201).json(usuario);
+    return res.status(201).json(usuario);
   } catch (erro) {
-    res.status(400).json({ erro: erro.message });
+    return res.status(400).json({ erro: erro.message });
+  }
+}
+//Isa, adicionei a partir daqui o bucarPorID, Atualizar e deletar
+async function buscarPorId(req, res) {
+  try {
+    const { id } = req.params;
+    const usuario = await usuariosService.buscarPorId(id);
+    
+    if (!usuario) {
+      return res.status(404).json({ erro: "Usuário não encontrado" });
+    }
+    
+    return res.json(usuario);
+  } catch (erro) {
+    return res.status(500).json({ erro: erro.message });
+  }
+}
+
+async function atualizar(req, res) {
+  try {
+    const { id } = req.params;
+    const usuarioAtualizado = await usuariosService.atualizar(id, req.body);
+    
+    if (!usuarioAtualizado) {
+      return res.status(404).json({ erro: "Usuário não encontrado" });
+    }
+    
+    return res.json(usuarioAtualizado);
+  } catch (erro) {
+    return res.status(400).json({ erro: erro.message });
+  }
+}
+
+async function deletar(req, res) {
+  try {
+    const { id } = req.params;
+    await usuariosService.deletar(id);
+  
+    return res.status(204).send();
+  } catch (erro) {
+    return res.status(500).json({ erro: erro.message });
   }
 }
 
 export default {
   listarTodos,
   criar,
+  buscarPorId,
+  atualizar,
+  deletar,
 };
